bfc2abbd00fb5559902f5a55394a49427d341c0f
rehma017 <rehma017@hotmail.com>
2016-03-25 14:26:35 +0000

    Added files via upload

---
+++ dockerinstall.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Docker installation script initiated"
+
+sudo apt-get update
+sudo apt-get install curl
+curl -fsSL https://get.docker.com/ | sh
+curl -fsSL https://get.docker.com/gpg | sudo apt-key add -
+docker run hello-world
